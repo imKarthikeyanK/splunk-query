ddd0165d10c4890ee93cef40fed27172316571b0
Karthikeyan K <superkarthi789@gmail.com>
2020-05-17 21:53:44 +0530

    changes and readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Splunk-Query - Flask Application
--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     # Configuration for Mail
-    MAIL_SERVER = ''
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
-    MAIL_DEFAULT_SENDER = ''
+    MAIL_USERNAME = "superkarthi098@gmail.com"
+    MAIL_PASSWORD = "password"
+    MAIL_DEFAULT_SENDER = "superkarthi098@gmail.com"
