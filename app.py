b0a4ce76f5217579be3c56139fa685afe313aaa0
Karthikeyan K <superkarthi789@gmail.com>
2020-05-17 12:14:39 +0530

    initial works

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    return render_template("index.htm")
+
+@app.route("/splunk-query", methods=['POST'])
+def splunk_query():
+    if request.method == "POST":
+        data = request.form
+        print(data)
+
     return render_template("index.htm")
 
 if __name__ == "__main__":
